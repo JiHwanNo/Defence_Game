66f7e7317c158455ed7065077f59192362de9778
JiHwanNo <83812895+JiHwanNo@users.noreply.github.com>
2022-01-15 22:00:37 +0900

    2022-01-15

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    GameObject player;
+    
+    public LayerMask unwalkableMask;
     Vector2 gridSize;
 
     Node[,] grid;
@@ -14,6 +14,8 @@ public class Grid : MonoBehaviour
     int gridXCount, gridYCount;   //Grid XÃà Node °¹¼ö, Grid YÃà Node °¹¼ö
     private void Awake()
     {
+        player = GameObject.Find("Player");
+
         nodeRadius = 0.5f;
         nodeDiameter = nodeRadius * 2f;
         gridSize = new Vector2(30, 30);
@@ -21,12 +23,10 @@ public class Grid : MonoBehaviour
         gridXCount = Mathf.RoundToInt(gridSize.x / nodeDiameter);
         gridYCount = Mathf.RoundToInt(gridSize.y / nodeDiameter);
 
-    }
-    private void Start()
-    {
         CreateGrid();
-    }
 
+    }
+   
     private void CreateGrid()
     {
         grid = new Node[gridXCount, gridYCount]; // [30,30]
@@ -39,21 +39,41 @@ public class Grid : MonoBehaviour
             {
                 Vector3 worldPosition = worldBottomLeft + new Vector3((x * nodeDiameter + nodeRadius), 0, (y * nodeDiameter + nodeRadius));
 
-                grid[x, y] = new Node(worldPosition, x, y);
+                bool walkable = !(Physics.CheckSphere(worldPosition, nodeRadius, unwalkableMask));
+
+                grid[x, y] = new Node(walkable, worldPosition, x, y);
             }
         }
 
+    }
+
+    Node GetNodeFromWorldPosition(Vector3 WP)
+    {
+        float percen_X = (WP.x + gridSize.x * 0.5f) / gridSize.x;
+        float percen_Y = (WP.z + gridSize.y * 0.5f) / gridSize.y;
+
+        percen_X = Mathf.Clamp01(percen_X);
+        percen_Y = Mathf.Clamp01(percen_Y);
+
+        
+        int x = Mathf.RoundToInt((gridXCount - 1) * percen_X);
+        int y = Mathf.RoundToInt((gridYCount - 1) * percen_Y);
+
+        return grid[x, y];
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, 1f, gridSize.y));
 
-        if(grid != null)
+        if (grid != null)
         {
-            foreach(var node in grid)
+            foreach (var node in grid)
             {
-                Gizmos.color = Color.white;
+                if (node == GetNodeFromWorldPosition(player.transform.position))
+                    Gizmos.color = Color.green;
+                else
+                    Gizmos.color = node.IsWalkable ? Color.white : Color.red;
 
                 Gizmos.DrawCube(node.worldPosition, Vector3.one * nodeDiameter);
             }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class Node
 {
+    public bool IsWalkable;
     public Vector3 worldPosition;
 
     public int gridX, gridY;
 
-    public Node(Vector3 pos, int x, int y)
+    public Node(bool walkable, Vector3 pos, int x, int y)
     {
+        this.IsWalkable = walkable;
         worldPosition = pos;
         gridX = x;
         gridY = y;
